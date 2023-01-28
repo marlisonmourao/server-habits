66305e234d0a8cc18ee24e50867c03942a010763
marlisonmourao <bentesmourao@gmail.com>
2023-01-28 17:37:19 -0400

    applying notifications

---
+++ src/notification-routes.ts
@@ -0,0 +1,28 @@
+import WebPush from 'web-push';
+import { FastifyInstance } from 'fastify';
+
+const publicKey =
+  "BIWS0kH1WMS66q3_3F48u8wN4t_CwQbPIm3vvLTLhh_WEAEwlE_BJcqIh5D84blwB48wTJ_pxq52s7J8QY-pwrU";
+const privateKey = "vbB4JiVMMPAomAy6k2zc4P_uEJMYW1JPUBwL2kDjjF4";
+
+WebPush.setVapidDetails("http://localhost:3333", publicKey, privateKey);
+
+export async function notificationRoutes(app: FastifyInstance) {
+  app.get("/push/public_key", () => {
+    return {
+      publicKey,
+    };
+  });
+
+  app.post('/push/register', (request, replay) => {
+    console.log(request.body)
+
+    return replay.status(201).send()
+  })
+
+  app.post('/push/send', async (request, replay) => {
+    console.log(request.body)
+
+    return replay.status(201).send()
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import './lib/dayjs'
 import Fastify from "fastify"
 import cors from '@fastify/cors'
 import { appRoutes } from "./routes"
+import { notificationRoutes } from './notification-routes'
 
 const app = Fastify()
 
 app.register(appRoutes)
 app.register(cors)
+app.register(notificationRoutes)
 
 app.listen({
   port: 3333,
+  host: '0.0.0.0'
 }).then(() => {
   console.log('Server running')
 })
