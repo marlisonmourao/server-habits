3808bf1d2a4a504ec4dc99992fcedb389278fb6e
marlisonmourao <bentesmourao@gmail.com>
2023-01-28 18:26:20 -0400

    nitifications

--- src/notification-routes.ts
+++ src/notification-routes.ts
@@ -1,28 +1,43 @@
 import WebPush from 'web-push';
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 
 const publicKey =
-  "BIWS0kH1WMS66q3_3F48u8wN4t_CwQbPIm3vvLTLhh_WEAEwlE_BJcqIh5D84blwB48wTJ_pxq52s7J8QY-pwrU";
-const privateKey = "vbB4JiVMMPAomAy6k2zc4P_uEJMYW1JPUBwL2kDjjF4";
+  'BIWS0kH1WMS66q3_3F48u8wN4t_CwQbPIm3vvLTLhh_WEAEwlE_BJcqIh5D84blwB48wTJ_pxq52s7J8QY-pwrU';
+const privateKey = 'vbB4JiVMMPAomAy6k2zc4P_uEJMYW1JPUBwL2kDjjF4';
 
-WebPush.setVapidDetails("http://localhost:3333", publicKey, privateKey);
+WebPush.setVapidDetails('http://localhost:3333', publicKey, privateKey);
 
 export async function notificationRoutes(app: FastifyInstance) {
-  app.get("/push/public_key", () => {
+  app.get('/push/public_key', () => {
     return {
       publicKey,
     };
   });
 
   app.post('/push/register', (request, replay) => {
-    console.log(request.body)
+    console.log(request.body);
 
-    return replay.status(201).send()
-  })
+    return replay.status(201).send();
+  });
 
   app.post('/push/send', async (request, replay) => {
-    console.log(request.body)
+    const sendPushBody = z.object({
+      subscription: z.object({
+        endpoint: z.string(),
+        keys: z.object({
+          p256dh: z.string(),
+          auth: z.string(),
+        }),
+      }),
+    });
 
-    return replay.status(201).send()
-  })
+    const { subscription } = sendPushBody.parse(request.body);
+
+    setTimeout(() => {
+    WebPush.sendNotification(subscription, 'Hello Do Backend');
+    }, 5000)
+
+    return replay.status(201).send();
+  });
 }
